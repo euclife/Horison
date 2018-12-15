cd59f28788f10f88904c98b8c6f559e596e841fa
Chandra Ramdhan Purnama <chandraramdhanpurnama@gmail.com>
2018-12-15 16:28:26 +0700

    Revert "Update index.php"
    This reverts commit 59487284f6e09a91a926f3509ac190e5db990754.

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="oi oi-menu"></span> Menu
       </button>
-hgfhgfhgfhgfhgfhgf
+
       <div class="collapse navbar-collapse" id="ftco-nav">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item
